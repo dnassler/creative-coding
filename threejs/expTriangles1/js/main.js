fcf4c9ca21f1bea2b051e0ba5d5375c39116fb30
Derek Nassler <dnassler@rogers.com>
2016-03-07 11:53:04 -0500

    expTriangles1: new base experiment start

---
+++ threejs/expTriangles1/js/main.js
@@ -0,0 +1,342 @@
+import 'babel-polyfill';
+import dat from 'dat-gui';
+import TWEEN from 'tween.js';
+import p5 from 'p5';
+
+//--
+//--
+// var sketch = function( p ) {
+//   p.preload = function() {
+//
+//   };
+//   p.setup = function() {
+//     //p.createCanvas(700, 410);
+//     p.noLoop();
+//   };
+//
+//   p.draw = function() {
+//     p.background(0);
+//     p.fill(255);
+//     p.rect(x,y,50,50);
+//   };
+// };
+// var myp5 = new p5(sketch);
+//--
+//--
+
+var scene, camera, renderer;
+var light, ambLight;
+var controlAttr, gui, stats;
+var ocontrols;
+var ground;
+
+var light1, light4;
+
+var shapeArr = [];
+function Shape( mesh ) {
+  var shapeSpaceWidth = 300;
+  var shapeSpaceHeight = 500;
+  function _move() {
+    var newPos = mesh.position.clone();
+    if ( Math.random() < 0.5 ) {
+      newPos.x = (Math.random()-0.5) * shapeSpaceWidth;
+    } else {
+      newPos.z = (Math.random()-0.5) * shapeSpaceHeight;
+    }
+
+    var dur = Math.random()*10000+1000;
+    var delayDur = Math.random()*10000;
+    var tween = new TWEEN.Tween( mesh.position )
+      .delay(delayDur)
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .to({ x: newPos.x, z: newPos.z }, dur)
+      .onComplete(function(){
+        _move();
+      });
+    var newRotationX = (Math.random()-0.5)*(Math.PI*4);
+    var tweenRotate = new TWEEN.Tween( mesh.rotation )
+      .delay(delayDur)
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .to({ y: newRotationX }, dur );
+
+    tween.start();
+    tweenRotate.start();
+  }
+  _move();
+}
+
+function init() {
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+  // var f = 1;
+  // camera = new THREE.OrthographicCamera( window.innerWidth /( - 16*f), window.innerWidth / (16*f), window.innerHeight / (16*f), window.innerHeight / - (16*f), -200, 1000 );
+
+  controlAttr = new function () {
+    this.groundPosY = -45;//-150;
+    this.intensityLight1 = 0;
+    this.intensityLight4 = 3.5;
+    this.shadowBiasLight4 = 0.5;
+    this.distanceLight4 = 600;
+    this.showStarField = true;
+    this.starFieldRotationSpeed = -1;
+    this.changeCameraViewPoint = function() {
+      nextCameraPos();
+    };
+  };
+  gui = new dat.GUI();
+  gui.add( controlAttr, 'groundPosY', -500, 100 ).onChange(function(v){ ground.position.y = v; });
+  gui.add( controlAttr, 'intensityLight1', 0, 1 ).onChange(function(v){ light1.intensity = v; });
+  gui.add( controlAttr, 'intensityLight4', 0, 10 ).onChange(function(v){ light4.intensity = v; });
+  gui.add( controlAttr, 'shadowBiasLight4', 0, 2 ).onChange(function(v){ light4.shadowBias = v; });
+  gui.add( controlAttr, 'distanceLight4', 0, 1000 ).onChange(function(v){ light4.distance = v;});
+  gui.add( controlAttr, 'showStarField' ).onChange(function(v){ skyBox.visible = !skyBox.visible; });
+  gui.add( controlAttr, 'starFieldRotationSpeed', -10, 10 );
+  gui.add( controlAttr, 'changeCameraViewPoint' );
+
+
+  // camera.position.set(-309.1,25.134,-446.843);
+  camera.zoom = 1;
+  camera.updateProjectionMatrix();
+  camera.position.z = 300;
+  // var lookAtVec = new THREE.Vector3(0,0,0);
+  // camera.lookAt(lookAtVec);
+
+  // ---
+  // ground
+  //
+
+  var getGroundTexture = function(w,h) {
+    var canvas = document.createElement('canvas');
+    canvas.width = w;;
+    canvas.height = h;;
+
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = "#a0adaf";
+    ctx.fillStyle = "#d0dddf";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+    // ctx.fillStyle = "rgb(0,0,255)";
+    // let bw = w/10;
+    // for (let i=0; i<10; i++){
+    //   ctx.fillRect(i*bw,i*bw,bw,bw);
+    // }
+
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    return texture;
+  }
+  var groundTexture = getGroundTexture(1024,1024);
+  // starTexture.wrapS = THREE.RepeatWrapping;
+  // starTexture.wrapT = THREE.RepeatWrapping;
+  // starTexture.repeat.set( 4, 4 );
+
+  var geometry = new THREE.BoxGeometry( 800, 0.15, 800 );
+  var material1 = new THREE.MeshPhongMaterial( {
+    color: 0xa0adaf,
+    shininess: 150,
+    specular: 0xffffff,
+    shading: THREE.SmoothShading
+  } );
+  var material2 = new THREE.MeshPhongMaterial({
+    shininess: 150,
+    specular: 0xffffff,
+    shading: THREE.SmoothShading,
+    map: groundTexture,
+    transparent: false,
+    opacity: 1//,
+    //blending: THREE.MultiplyBlending
+  });
+  // var material2 = new THREE.MeshBasicMaterial({
+  //   map: groundTexture,
+  //   transparent: true,
+  //   opacity: 0.5//,
+  //   //blending: THREE.MultiplyBlending
+  // });
+  ground = new THREE.Mesh( geometry, material2 );
+  //ground = THREE.SceneUtils.createMultiMaterialObject( geometry, [material2, material1]);
+  //ground.scale.multiplyScalar( 1 );
+  ground.position.set(0, controlAttr.groundPosY, 0);
+  ground.castShadow = true;
+  ground.receiveShadow = true;
+  scene.add( ground );
+
+  //
+  // end of ground
+  // ---
+
+
+  function addShapes( numShapes ){
+    var size = 1;
+    var geometry = new THREE.CylinderGeometry( 0, 10, 30*size, 4, 1 );
+    var material =  new THREE.MeshPhongMaterial( {
+      shininess: 150,
+      specular: 0xffffff,
+      color:0x0000ff,
+      shading: THREE.FlatShading } );
+
+    for ( var i = 0; i < numShapes; i ++ ) {
+
+      var mesh = new THREE.Mesh( geometry, material );
+      mesh.position.x = ( Math.random() - 0.5 ) * 300;
+      mesh.position.y = 0;//( Math.random() - 0.5 ) * 1000;
+      mesh.position.z = ( Math.random() - 0.5 ) * 500;
+      //mesh.updateMatrix();
+      //mesh.matrixAutoUpdate = false;
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+
+      scene.add( mesh );
+
+      shapeArr.push( new Shape(mesh) );
+
+
+    }
+  }
+  addShapes(2);
+
+  var sphere = new THREE.SphereGeometry( 10, 16, 8 );
+
+  light1 = new THREE.DirectionalLight( 0xffffff, controlAttr.intensityLight1 );
+  light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffffff } ) ) );
+  light1.position.set( 0, 100, 0 );//.normalize();
+  scene.add( light1 );
+  // light1.intensity = 0; // to make the light not be visible
+  // light1.castShadow = true;
+  // light1.shadowBias = 0.01;
+  // light1.shadowDarkness = 0.5;
+  // light1.shadowCameraNear = 1;
+  // light1.shadowCameraFar = 1000;
+  // light1.shadowMapWidth = 1024;//4096;//2048;//1024;
+  // light1.shadowMapHeight = 1024;//4096;//2048;//1024;
+  light1.target = ground;
+
+  light4 = new THREE.PointLight( 0x808080, controlAttr.intensityLight4, 1000 );
+  light4.castShadow = true;
+  light4.shadowBias = controlAttr.shadowBiasLight4;
+  light4.shadowCameraNear = 1;
+  light4.shadowCameraFar = 1000;
+  light4.shadowMapWidth = 1024;
+  light4.shadowMapHeight = 1024;
+  var sphere = new THREE.SphereGeometry( 10, 16, 8 );
+  light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffffff } ) ) );
+  light4.position.set(0,100,0);
+  scene.add( light4 );
+  light4.target = ground;
+
+
+  // ---
+  // sky SphereGeometry
+
+  // create a star field algorithmically
+
+  var getStarsTexture = function() {
+    var canvas = document.createElement('canvas');
+    canvas.width = 3000;
+    canvas.height = 1000;
+
+    var ctx = canvas.getContext('2d');
+    // draw the stars
+    for (let i=0; i<500; i++){
+      ctx.fillStyle = "rgb(255,255,255)";
+      ctx.beginPath();
+      ctx.arc(THREE.Math.randInt(0,canvas.width), THREE.Math.randInt(0,canvas.height), 1, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    return texture;
+  }
+  var getStarsTexture2 = function() {
+    var g = p5.createGraphics();
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    return texture;
+  }
+
+
+  // ---
+
+  // load a texture, set wrap mode to repeat
+  //var textureMilkyWay = new THREE.TextureLoader().load( "eso0932a.jpg" );
+  // texture.wrapS = THREE.RepeatWrapping;
+  // texture.wrapT = THREE.RepeatWrapping;
+  // texture.repeat.set( 4, 4 );
+
+  var geometry = new THREE.SphereGeometry(3000, 60, 40);
+  var uniforms = {
+    // texture: { type: 't', value: textureMilkyWay }
+    texture: { type: 't', value: getStarsTexture() }
+  };
+
+  var material = new THREE.ShaderMaterial( {
+    uniforms:       uniforms,
+    vertexShader:   document.getElementById('sky-vertex').textContent,
+    fragmentShader: document.getElementById('sky-fragment').textContent
+  });
+
+  var skyBox = new THREE.Mesh(geometry, material);
+  skyBox.scale.set(-1, 1, 1);
+  skyBox.eulerOrder = 'XZY';
+  skyBox.renderDepth = 1000.0;
+  scene.add(skyBox);
+
+  //
+  // ---
+
+  ambLight = new THREE.AmbientLight( 0x202020 ); // soft white light
+  scene.add( ambLight );
+
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor( 0x000000 );
+  renderer.shadowMap.enabled = true;
+  //renderer.shadowMap.type = THREE.BasicShadowMap;
+  renderer.shadowMap.needsUpdate = true;
+
+  document.body.appendChild( renderer.domElement );
+
+  ocontrols = new THREE.OrbitControls( camera, renderer.domElement );
+  // ocontrols.target.set( 0, 0, 0 );
+  // ocontrols.update();
+
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild( stats.domElement );
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  var cameraIndex = 0;
+  var cameraPosArr = [new THREE.Vector3(0,0,300),new THREE.Vector3(300,0,0),new THREE.Vector3(-300,0,0),new THREE.Vector3(0,300,0)];
+  function nextCameraPos() {
+    camera.position.copy( cameraPosArr[cameraIndex] );
+    cameraIndex += 1;
+    cameraIndex %= cameraPosArr.length;
+  }
+
+};
+
+init();
+animate();
+
+function animate() {
+  requestAnimationFrame( animate );
+  ocontrols.update();
+  TWEEN.update();
+  render();
+}
+
+function render() {
+  renderer.render( scene, camera );
+  stats.update();
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
---
+++ threejs/expTriangles1/webpack.config.js
@@ -0,0 +1,53 @@
+/* global __dirname */
+
+var path = require('path');
+
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var dir_js = path.resolve(__dirname, 'js');
+var dir_html = path.resolve(__dirname, 'html');
+var dir_build = path.resolve(__dirname, 'build');
+
+module.exports = {
+    entry: path.resolve(dir_js, 'main.js'),
+    output: {
+        path: dir_build,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: dir_build,
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: dir_js,
+            }
+        ]
+    },
+
+    // resolve: {
+    //     root: [path.join(__dirname, "bower_components")]
+    // },
+
+    plugins: [
+        // new webpack.ResolverPlugin(
+        //     new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
+        // ),
+
+        // Simply copies the files over
+        new CopyWebpackPlugin([
+            { from: dir_html } // to: output.path
+        ]),
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+};
